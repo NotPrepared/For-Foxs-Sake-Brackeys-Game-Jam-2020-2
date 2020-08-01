67d715e030fab8a554252028056a9f3484da8054
Janik Mayr <jan.222.ik@gmail.com>
2020-08-01 21:15:29 +0200

    Added GameController with UI to display time left

---
+++ Assets/Scripts/game_controller/GameController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+// ReSharper disable once CheckNamespace
+public class GameController : MonoBehaviour
+{
+    public static GameController Instance;
+
+    public List<TextMeshProUGUI> timerTextOuts;
+
+    private ITimer timer;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    private void Start()
+    {
+        timer = TimerImpl.Instance;
+        timer.addTimeConsumer(time => { timerTextOuts.ForEach(item => item.text = $"{time:0.###}"); });
+        timer.resumeTimer();
+    }
+}
