e33a4ae93385de45909cad4db0c16ce853a2022c
Janik Mayr <jan.222.ik@gmail.com>
2020-08-01 17:16:37 +0200

    Added Basic Menu with conditional logic for button behavior, Introduced TextMesh PRO

---
+++ Assets/Scripts/debug/TODO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using UnityEngine;
+
+namespace debug
+{
+    public static class TODO
+    {
+        public static void asException(string msg) => throw new TODO_Exception(msg);
+        public static void asLogWarning(string msg) => Debug.LogWarning(msg);
+
+        private class TODO_Exception : Exception
+        {
+            public TODO_Exception(string msg) : base(msg)
+            { /*Ignored*/ }
+        }
+    }
+}
---
+++ Assets/Scripts/menus/MainMenuController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+// ReSharper disable once CheckNamespace
+public class MainMenuController : MonoBehaviour
+{
+    public Button newGameORResetBtn;
+    public TMP_Text newGameORResetBtnLabel;
+    public Button continueBtn;
+
+    public Button sandboxBtn;
+    
+    private static void openLevel(string sceneName) => SceneManager.LoadScene(sceneName);
+    
+    /// <summary>
+    /// Resets progress and starts a new game
+    /// </summary>
+    private static void newGame() {
+        PersistenceHandler.resetGameProgress();
+        PersistenceHandler.startGame();
+        // If it does not launch immediately invoke updateUI() 
+    }
+
+    /// <summary>
+    /// Continues a active game at last scene
+    /// </summary>
+    private static void continueGame()
+    {
+        PersistenceHandler.continueGame();
+    }
+
+    /// <summary>
+    /// Prepares and launches Sandbox Scene
+    /// </summary>
+    private static void openSandbox()
+    {
+        openLevel("Sandbox");
+    }
+
+    private void Start()
+    {
+        newGameORResetBtn.onClick.AddListener(newGame);
+        sandboxBtn.onClick.AddListener(openSandbox);
+        updateUI();
+    }
+
+    private void updateUI() {
+        // Check if an active Game exists
+        if (PersistenceHandler.hasActiveGame())
+        {
+            newGameORResetBtnLabel.text = "Reset Game";
+            continueBtn.gameObject.SetActive(true);
+            continueBtn.onClick.AddListener(continueGame);
+        }
+        else
+        {
+            newGameORResetBtnLabel.text = "New Game";
+            continueBtn.gameObject.SetActive(false);
+            continueBtn.onClick.RemoveAllListeners();
+        }
+    }
+}
