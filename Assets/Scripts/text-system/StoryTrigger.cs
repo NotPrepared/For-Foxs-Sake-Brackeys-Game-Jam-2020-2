bfa2aea6fcd491f4cca074bad92853c423bfcf13
Janik Mayr <jan.222.ik@gmail.com>
2020-08-05 16:11:00 +0200

    Added Story text system to GameController prfab and implementend some triggers for level 1

---
+++ Assets/Scripts/text-system/StoryTrigger.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using UnityEngine;
+
+
+public class StoryTrigger : MonoBehaviour
+{
+    [SerializeField] private string text;
+    [SerializeField] private float disappearAfter;
+    [SerializeField] private bool requiresSpecificLayer;
+    [SerializeField] private bool requiresLayerNow;
+    [SerializeField] private bool requiresOtherTriggerFired;
+    [SerializeField] private StoryTrigger requiredTrigger;
+
+    [HideInInspector]
+    public bool hasTriggered;
+
+    private void Awake()
+    {
+        if (requiredTrigger == null)
+        {
+            if (requiresOtherTriggerFired)
+            {
+                throw new MissingReferenceException("IF Trigger is required than a instance must be passed.");
+            }
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        if (hasTriggered) return;
+        if (!other.CompareTag("Player")) return;
+        if (requiresSpecificLayer)
+        {
+            if (GameController.Instance.isPresent != requiresLayerNow)
+            {
+                return;
+            }
+        }
+
+        if (requiresOtherTriggerFired)
+        {
+            if (requiredTrigger.hasTriggered == false)
+            {
+                return;
+            }
+        }
+
+        TextDisplayController.Instance.setDisplayText(text, disappearAfter);
+        hasTriggered = true;
+    }
+}
---
+++ Assets/Scripts/text-system/TextDisplayController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+
+
+public class TextDisplayController : MonoBehaviour
+{
+    public static TextDisplayController Instance;
+    [SerializeField] private TextMeshProUGUI display;
+    [SerializeField] private GameObject textDisplayRoot;
+
+    private float fadeAfter;
+    private bool hasContent;
+
+
+    private void Awake()
+    {
+        if (Instance == null) Instance = this;
+        setDisplayText("", 0);
+    }
+
+    private void FixedUpdate()
+    {
+        if (!hasContent) return;
+        fadeAfter -= Time.fixedDeltaTime;
+        if (fadeAfter > 0) return;
+        // Delete text
+        textDisplayRoot.SetActive(false);
+        hasContent = false;
+    }
+
+    public void setDisplayText(string text, float fadeTime)
+    {
+        hasContent = true;
+        fadeAfter = fadeTime;
+        display.text = text;
+       textDisplayRoot.SetActive(true);
+    }
+}
